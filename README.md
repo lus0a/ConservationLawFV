79f19f33f1816bc545e280a5eb34c14e12ee6eec
Dmitriy Logashenko <LogashenkoDL@mail.ru>
2023-06-06 15:10:32 +0300

    The initial commit of the new plugin.

---
+++ README.md
@@ -0,0 +1,8 @@
+# ConservationLawFV #
+
+**UG4-Plugin** implementing the vertex centered FV discretization (based on the baricentric secondary grid) for a general conservation law.
+
+Copyright (c) 2023
+
+Please install/clone this repository through UG4's package manager
+[ughub](https://github.com/UG4/ughub).
---
+++ clfv_plugin.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2023
+ * Author: Dmitry Logashenko, Shuai Lu
+ * 
+ * This file is part of UG4.
+ * 
+ * UG4 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 3 (as published by the
+ * Free Software Foundation) with the following additional attribution
+ * requirements (according to LGPL/GPL v3 §7):
+ * 
+ * (1) The following notice must be displayed in the Appropriate Legal Notices
+ * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
+ * 
+ * (2) The following notice must be displayed at a prominent place in the
+ * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
+ * 
+ * (3) The following bibliography is recommended for citation and must be
+ * preserved in all covered files:
+ * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
+ *   parallel geometric multigrid solver on hierarchically distributed grids.
+ *   Computing and visualization in science 16, 4 (2013), 151-164"
+ * "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
+ *   flexible software system for simulating pde based models on high performance
+ *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+/**
+ * Plugin of the vertex-centered FV-discretizations of general conservation laws.
+ */
+
+/* ug headers: */
+#include "bridge/util.h"
+#include "bridge/util_domain_algebra_dependent.h"
+
+/* general tools for the Nedelec element */
+
+/* discretizations' headers: */
+
+using namespace std;
+using namespace ug::bridge;
+
+namespace ug{
+namespace ConserationLawFV{
+
+struct Functionality
+{
+	/**
+	 * Function called for the registration of Domain dependent parts.
+	 * All Functions and Classes depending on the Domain
+	 * are to be placed here when registering. The method is called for all
+	 * available Domain types, based on the current build options.
+	 *
+	 * @param reg	registry
+	 * @param grp	group for sorting of functionality
+	 */
+	template <typename TDomain>
+	static void Domain(Registry& reg, string grp)
+	{
+	//	string suffix = GetDomainSuffix<TDomain>();
+	//	string tag = GetDomainTag<TDomain>();
+		
+	// Parameter specification tools
+	}
+	
+	/**
+	 * Function called for the registration of Domain and Algebra dependent parts.
+	 * All Functions and Classes depending on both Domain and Algebra
+	 * are to be placed here when registering. The method is called for all
+	 * available Domain and Algebra types, based on the current build options.
+	 *
+	 * @param reg	registry
+	 * @param grp	group for sorting of functionality
+	 */
+	template <typename TDomain, typename TAlgebra>
+	static void DomainAlgebra(Registry& reg, string grp)
+	{
+	//	static const int dim = TDomain::dim;
+	//	string suffix = GetDomainAlgebraSuffix<TDomain,TAlgebra>();
+	//	string tag = GetDomainAlgebraTag<TDomain,TAlgebra>();
+	};
+	
+};
+
+} // end namespace ConserationLawFV
+
+/**
+ * This function is called when the plugin is loaded.
+ */
+extern "C" void
+InitUGPlugin_ConservationLawFV(Registry* reg, string grp)
+{
+	grp.append("/SpatialDisc/ConservationLawFV");
+	typedef ConserationLawFV::Functionality Functionality;
+
+	try
+	{
+		RegisterDomainDependent<Functionality>(*reg,grp);
+		RegisterDomainAlgebraDependent<Functionality>(*reg,grp);
+	}
+	UG_REGISTRY_CATCH_THROW(grp);
+}
+
+} // namespace ug
+
+/* End of File */
