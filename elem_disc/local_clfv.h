a7911c3639f9d0fd3fbf4bf5fe1ee829534a517d
Shuai Lu <shuai.lu@kaust.edu.sa>
2023-07-16 14:59:51 +0300

    Flux implementation

--- elem_disc/local_clfv.h
+++ elem_disc/local_clfv.h
@@ -2,6 +2,8 @@
  * Copyright (c) 2023:
  * Author: Dmitry Logashenko, Shuai Lu
  * 
+ * Based on ConvectionDiffusion
+ * 
  * This file is part of UG4.
  * 
  * UG4 is free software: you can redistribute it and/or modify it under the
@@ -48,7 +50,6 @@
 #include "lib_disc/function_spaces/grid_function.h"
 
 /* Discretization's headers: */
-#include "lib_disc/spatial_disc/disc_util/conv_shape_interface.h"
 
 namespace ug{
 namespace Conservation_Law_FV{
@@ -99,75 +100,49 @@ public:
 		const char * subsets ///< subsets where to assemble
 	);
 	
-///	to compute the upwind shapes
-	void set_upwind(SmartPtr<IConvectionShapes<dim> > shapes);
-	
 protected:
 	void init_imports();
 	
-	/// method to compute the upwind shapes
-		SmartPtr<IConvectionShapes<dim> > m_spConvShape;
-
-	///	returns the updated convection shapes
-		typedef IConvectionShapes<dim> conv_shape_type;
-		const IConvectionShapes<dim>& get_updated_conv_shapes(const FVGeometryBase& geo, bool compute_deriv);
-		
 			///	computes the concentration
-		template <typename TElem>
-		void ex_value(number vValue[],
-		              const MathVector<dim> vGlobIP[],
-		              number time, int si,
-		              const LocalVector& u,
-		              GridObject* elem,
-		              const MathVector<dim> vCornerCoords[],
-		              const MathVector<FV1Geometry<TElem, dim>::dim> vLocIP[],
-		              const size_t nip,
-		              bool bDeriv,
-		              std::vector<std::vector<number> > vvvDeriv[]);
+	template <typename TElem>
+	void ex_value(number vValue[],
+					const MathVector<dim> vGlobIP[],
+					number time, int si,
+					const LocalVector& u,
+					GridObject* elem,
+					const MathVector<dim> vCornerCoords[],
+					const MathVector<FV1Geometry<TElem, dim>::dim> vLocIP[],
+					const size_t nip,
+					bool bDeriv,
+					std::vector<std::vector<number> > vvvDeriv[]);
 
 	///	computes the gradient of the concentration
-		template <typename TElem>
-		void ex_grad(MathVector<dim> vValue[],
-		             const MathVector<dim> vGlobIP[],
-		             number time, int si,
-		             const LocalVector& u,
-		             GridObject* elem,
-		             const MathVector<dim> vCornerCoords[],
-		             const MathVector<FV1Geometry<TElem, dim>::dim> vLocIP[],
-		             const size_t nip,
-		             bool bDeriv,
-		             std::vector<std::vector<MathVector<dim> > > vvvDeriv[]);
+	template <typename TElem>
+	void ex_grad(MathVector<dim> vValue[],
+					const MathVector<dim> vGlobIP[],
+					number time, int si,
+					const LocalVector& u,
+					GridObject* elem,
+					const MathVector<dim> vCornerCoords[],
+					const MathVector<FV1Geometry<TElem, dim>::dim> vLocIP[],
+					const size_t nip,
+					bool bDeriv,
+					std::vector<std::vector<MathVector<dim> > > vvvDeriv[]);
 	
 public:
-///	sets the diffusion tensor
-	/**
-	 * This method sets the Diffusion tensor used in computations. If no
-	 * Tensor is set, a zero value is assumed.
-	 */
-	///	\{
-		void set_diffusion(SmartPtr<CplUserData<MathMatrix<dim, dim>, dim> > user);
-		void set_diffusion(number val);
-#ifdef UG_FOR_LUA
-		void set_diffusion(const char* fctName);
-		void set_diffusion(LuaFunctionHandle fct);
-#endif
-	///	\}
-	
-	
 	///	sets the flux
 	/**
 	 * This method sets the Flux. If no field is provided a zero
 	 * value is assumed.
 	 */
 	/// \{
-		void set_flux(SmartPtr<CplUserData<MathVector<dim>, dim> > user);
-		void set_flux(const std::vector<number>& vVel);
+	void set_flux(SmartPtr<CplUserData<MathVector<dim>, dim> > user);
+	void set_flux(const std::vector<number>& vVel);
 #ifdef UG_FOR_LUA
-		void set_flux(const char* fctName);
-		void set_flux(LuaFunctionHandle fct);
+	void set_flux(const char* fctName);
+	void set_flux(LuaFunctionHandle fct);
 #endif
 	/// \}	
-	
 	
 	///	sets the source / sink term
 	/**
@@ -175,40 +150,37 @@ public:
 	 * default.
 	 */
 	///	\{
-		void set_source(SmartPtr<CplUserData<number, dim> > user);
-		void set_source(number val);
+	void set_source(SmartPtr<CplUserData<number, dim> > user);
+	void set_source(number val);
 #ifdef UG_FOR_LUA
-		void set_source(const char* fctName);
-		void set_source(LuaFunctionHandle fct);
+	void set_source(const char* fctName);
+	void set_source(LuaFunctionHandle fct);
 #endif
 	///	\}
 	
-protected:
-	///	Data import for Diffusion
-		DataImport<MathMatrix<dim,dim>, dim> m_imDiffusion;
-		
+protected:		
 	///	Data import for the Flux
-		DataImport<MathVector<dim>, dim > m_imFlux;
+	DataImport<MathVector<dim>, dim > m_imFlux;
 		
 	///	Data import for the right-hand side (volume)
-		DataImport<number, dim> m_imSource;
+	DataImport<number, dim> m_imSource;
 
 public:
-		typedef SmartPtr<CplUserData<number, dim> > NumberExport;
-		typedef SmartPtr<CplUserData<MathVector<dim>, dim> > GradExport;
+	typedef SmartPtr<CplUserData<number, dim> > NumberExport;
+	typedef SmartPtr<CplUserData<MathVector<dim>, dim> > GradExport;
 
 	///	returns the export of the value of associated unknown function
-		SmartPtr<CplUserData<number, dim> > value();
+	SmartPtr<CplUserData<number, dim> > value();
 
 	///	returns the export of the gradient of associated unknown function
-		SmartPtr<CplUserData<MathVector<dim>, dim> > gradient();
+	SmartPtr<CplUserData<MathVector<dim>, dim> > gradient();
 
 protected:
 	///	Export for the concentration
-		SmartPtr<DataExport<number, dim> > m_exValue;
+	SmartPtr<DataExport<number, dim> > m_exValue;
 
 	///	Export for the gradient of concentration
-		SmartPtr<DataExport<MathVector<dim>, dim> > m_exGrad;
+	SmartPtr<DataExport<MathVector<dim>, dim> > m_exGrad;
 		
 
 //---- Local discretization interface: ----
@@ -249,34 +221,29 @@ private:
 	
 	
 protected:
-	///	computes the linearized defect w.r.t to the diffusion
-		template <typename TElem>
-		void lin_def_diffusion(const LocalVector& u,
-		                       std::vector<std::vector<MathMatrix<dim,dim> > > vvvLinDef[],
-		                       const size_t nip);
-
 	///	computes the linearized defect w.r.t to the flux
-		template <typename TElem>
-		void lin_def_flux(const LocalVector& u,
-		                  std::vector<std::vector<MathVector<dim> > > vvvLinDef[],
-		                  const size_t nip);
+	template <typename TElem>
+	void lin_def_flux(const LocalVector& u,
+						std::vector<std::vector<MathVector<dim> > > vvvLinDef[],
+						const size_t nip);
 						  
 	///	computes the linearized defect w.r.t to the source term
-		template <typename TElem>
-		void lin_def_source(const LocalVector& u,
-		                    std::vector<std::vector<number> > vvvLinDef[],
-		                    const size_t nip);
+	template <typename TElem>
+	void lin_def_source(const LocalVector& u,
+						std::vector<std::vector<number> > vvvLinDef[],
+						const size_t nip);
 
 //---- Registration of the template functions: ----
 private:
 	
 	void register_all_loc_discr_funcs();
 
-	struct RegisterLocalDiscr {
-			RegisterLocalDiscr(this_type * pThis) : m_pThis(pThis){}
-			this_type * m_pThis;
-			template< typename TElem > void operator() (TElem &)
-			{m_pThis->register_loc_discr_func<TElem> ();}
+	struct RegisterLocalDiscr 
+	{
+		RegisterLocalDiscr(this_type * pThis) : m_pThis(pThis){}
+		this_type * m_pThis;
+		template< typename TElem > void operator() (TElem &)
+		{m_pThis->register_loc_discr_func<TElem> ();}
 	};
 
 	template <typename TElem>
--- elem_disc/local_clfv_impl.h
+++ elem_disc/local_clfv_impl.h
@@ -2,6 +2,8 @@
  * Copyright (c) 2023:
  * Author: Dmitry Logashenko, Shuai Lu
  * 
+ * Based on ConvectionDiffusion
+ * 
  * This file is part of UG4.
  * 
  * UG4 is free software: you can redistribute it and/or modify it under the
@@ -38,7 +40,6 @@
 #include "common/util/provider.h"
 #include "lib_disc/spatial_disc/disc_util/geom_provider.h"
 #include "lib_disc/spatial_disc/disc_util/fv1_geom.h"
-#include "lib_disc/spatial_disc/disc_util/conv_shape.h"
 #ifdef UG_FOR_LUA
 #include "bindings/lua/lua_user_data.h"
 #endif
@@ -84,11 +85,7 @@ void ConservationLawFV<TDomain>::prepare_element_loop
 	ReferenceObjectID roid, ///< only elements with this roid are looped over
 	int si ///< and only in this subdomain
 )
-{
-	if(m_spConvShape.invalid())
-		UG_THROW("ConservationLawFV::prepare_element_loop:"
-						" Upwind has not been set.");
-						
+{						
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 	static const int refDim = TElem::dim;
@@ -97,14 +94,9 @@ void ConservationLawFV<TDomain>::prepare_element_loop
 	const size_t numSCVFip = geo.num_scvf_ips();
 	const MathVector<refDim>* vSCVip = geo.scv_local_ips();
 	const size_t numSCVip = geo.num_scv_ips();
-	m_imDiffusion.template 		set_local_ips<refDim>(vSCVFip,numSCVFip, false);
+
 	m_imFlux.template 			set_local_ips<refDim>(vSCVFip,numSCVFip, false);		
 	m_imSource.template 		set_local_ips<refDim>(vSCVip,numSCVip, false);
-		//	init upwind for element type
-	if(!m_spConvShape->template set_geometry_type<TFVGeom>(geo))
-		UG_THROW("ConservationLawFV::prep_elem_loop:"
-					" Cannot init upwind for element type.");
-	
 }
 
 /// finalizes the loop over the elements
@@ -127,8 +119,6 @@ void ConservationLawFV<TDomain>::prepare_element
 {
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	
-	TElem * pElem = static_cast<TElem*> (elem);
-	
 // 	update the FV geometry for this element
 	TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 	try
@@ -142,7 +132,7 @@ void ConservationLawFV<TDomain>::prepare_element
 	const size_t numSCVFip = geo.num_scvf_ips();
 	const MathVector<dim>* vSCVip = geo.scv_global_ips();
 	const size_t numSCVip = geo.num_scv_ips();
-	m_imDiffusion.			set_global_ips(vSCVFip, numSCVFip);
+	
 	m_imFlux.				set_global_ips(vSCVFip, numSCVFip);	
 	m_imSource.				set_global_ips(vSCVip, numSCVip);
 }
@@ -187,67 +177,25 @@ void ConservationLawFV<TDomain>::ass_dA_elem
 	typedef typename reference_element_traits<TElem>::reference_element_type ref_elem_type;
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	
-	TElem * pElem = static_cast<TElem*> (elem);
-	
 // 	Get finite volume geometry
 	const TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 
     const size_t numSh = geo.num_sh();
 	const size_t numScvf = geo.num_scvf();
-	
 
-//	get conv shapes
-//	const IConvectionShapes<dim>& convShape = get_updated_conv_shapes(geo, false);
-
-	if(m_imDiffusion.data_given()|| m_imFlux.data_given())
-	{
 	// 	loop Sub Control Volume Faces (SCVF)
-		for(size_t ip = 0; ip < numScvf; ++ip)
-		{
+	for(size_t ip = 0; ip < numScvf; ++ip)
+	{
 		// 	get current SCVF
-			const typename TFVGeom::SCVF& scvf = geo.scvf(ip);
+		const typename TFVGeom::SCVF& scvf = geo.scvf(ip);
 
-		/////////////////////////////////////////////////////
-		// Diffusive Term
-		/////////////////////////////////////////////////////
-			if(m_imDiffusion.data_given())
-			{
-			//	to compute D \nabla c
-				MathVector<dim> Dgrad_c, grad_c;
+		//	sum up flux
+		const number flux = VecDot(m_imFlux[ip], scvf.normal());
 
-			// 	compute gradient and shape at ip
-				VecSet(grad_c, 0.0);
-				for(size_t sh = 0; sh < scvf.num_sh(); ++sh)
-					VecScaleAppend(grad_c, u(_C_,sh), scvf.global_grad(sh));
-
-			//	scale by diffusion tensor
-				MatVecMult(Dgrad_c, m_imDiffusion[ip], grad_c);
-
-			// 	Compute flux
-				const number diff_flux = VecDot(Dgrad_c, scvf.normal());
-
-			// 	Add to local defect
-				d(_C_, scvf.from()) -= diff_flux;
-				d(_C_, scvf.to()  ) += diff_flux;
-			}
-			
-		/////////////////////////////////////////////////////
-		// Flux Term
-		/////////////////////////////////////////////////////			
-			if(m_imFlux.data_given())
-			{
-			//	sum up flux
-				const number flux = VecDot(m_imFlux[ip], scvf.normal());
-
-			//  add to local defect
-				d(_C_, scvf.from()) += flux;
-				d(_C_, scvf.to()  ) -= flux;
-				// For debug
-				//flux_Flux = flux;
-			}
-		}//end loop
-	
-	}
+		//  add to local defect
+		d(_C_, scvf.from()) += flux;
+		d(_C_, scvf.to()  ) -= flux;
+	}//end loop
 }
 
 /// assembles the local Jacobian of the stiffness part
@@ -264,66 +212,13 @@ void ConservationLawFV<TDomain>::ass_JA_elem
 	typedef typename reference_element_traits<TElem>::reference_element_type ref_elem_type;
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	
-	TElem * pElem = static_cast<TElem*> (elem);
-	
 // 	Get finite volume geometry
 	const TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 
     const size_t numSh = geo.num_sh();
 	const size_t numScvf = geo.num_scvf();
-	
-//	Diff. Tensor times Gradient
-	MathVector<dim> Dgrad;
 
-//	get conv shapes
-//	const IConvectionShapes<dim>& convShape = get_updated_conv_shapes(geo, false);
 
-//	Diffusion and Velocity Term
-	if(m_imDiffusion.data_given())
-	{
-	// 	loop Sub Control Volume Faces (SCVF)
-		for(size_t ip = 0; ip < numScvf; ++ip)
-		{
-		// 	get current SCVF
-			const typename TFVGeom::SCVF& scvf = geo.scvf(ip);
-		////////////////////////////////////////////////////
-		// Diffusive Term
-		////////////////////////////////////////////////////
-			if(m_imDiffusion.data_given())
-			{
-				#ifdef UG_ENABLE_DEBUG_LOGS
-				//	DID_CONV_DIFF_MP
-				number D_diff_flux_sum = 0.0;
-				#endif
-
-			// 	loop shape functions
-				for(size_t sh = 0; sh < scvf.num_sh(); ++sh)
-				{
-				// 	Compute Diffusion Tensor times Gradient
-					MatVecMult(Dgrad, m_imDiffusion[ip], scvf.global_grad(sh));
-
-				//	Compute flux at IP
-					const number D_diff_flux = VecDot(Dgrad, scvf.normal());
-					//UG_DLOG(DID_CONV_DIFF_MP, 2, ">>OCT_DISC_DEBUG: " << "local_clfv_impl.h: " << "ass_JA_elem(): " << "sh # "  << sh << " ; normalSize scvf # " << ip << ": " << VecLength(scvf.normal()) << "; \t from "<< scvf.from() << "; to " << scvf.to() << "; D_diff_flux: " << D_diff_flux << "; scvf.global_grad(sh): " << scvf.global_grad(sh) << std::endl);
-
-				// 	Add flux term to local matrix // HIER MATRIXINDIZES!!!
-					UG_ASSERT((scvf.from() < J.num_row_dof(_C_)) && (scvf.to() < J.num_col_dof(_C_)),
-							  "Bad local dof-index on element with object-id " << elem->base_object_id()
-							  << " with center: " << CalculateCenter(elem, vCornerCoords));
-
-					J(_C_, scvf.from(), _C_, sh) -= D_diff_flux;
-					J(_C_, scvf.to()  , _C_, sh) += D_diff_flux;
-
-					#ifdef UG_ENABLE_DEBUG_LOGS
-					//	DID_CONV_DIFF_MP
-					D_diff_flux_sum += D_diff_flux;
-					#endif
-				}
-
-				//UG_DLOG(DID_CONV_DIFF_MP, 2, "D_diff_flux_sum = " << D_diff_flux_sum << std::endl << std::endl);
-			}
-		}// end loop
-	}
 }
 
 /// assembles the mass part of the local defect
@@ -364,8 +259,6 @@ void ConservationLawFV<TDomain>::ass_rhs_elem
 {
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	
-	TElem * pElem = static_cast<TElem*> (elem);
-	
 // 	Get finite volume geometry
 	const TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 
@@ -385,59 +278,6 @@ void ConservationLawFV<TDomain>::ass_rhs_elem
 	}
 }
 
-
-
-//	computes the linearized defect w.r.t to the diffusion
-template<typename TDomain>
-template <typename TElem>
-void ConservationLawFV<TDomain>::
-lin_def_diffusion(const LocalVector& u,
-                  std::vector<std::vector<MathMatrix<dim,dim> > > vvvLinDef[],
-                  const size_t nip)
-{
-	typedef FV1Geometry<TElem, dim> TFVGeom;
-	
-//  get finite volume geometry
-	static const TFVGeom& geo = GeomProvider<TFVGeom>::get();
-
-//	get conv shapes
-	const IConvectionShapes<dim>& convShape = get_updated_conv_shapes(geo, true);
-
-//	reset the values for the linearized defect
-	for(size_t ip = 0; ip < nip; ++ip)
-		for(size_t c = 0; c < vvvLinDef[ip].size(); ++c)
-			for(size_t sh = 0; sh < vvvLinDef[ip][c].size(); ++sh)
-				vvvLinDef[ip][c][sh] = 0.0;
-
-//  loop Sub Control Volume Faces (SCVF)
-	for(size_t ip = 0; ip < geo.num_scvf(); ++ip)
-	{
-	// get current SCVF
-		const typename TFVGeom::SCVF& scvf = geo.scvf(ip);
-
-	// 	compute gradient at ip
-		MathVector<dim> grad_u;	VecSet(grad_u, 0.0);
-		for(size_t sh = 0; sh < scvf.num_sh(); ++sh)
-			VecScaleAppend(grad_u, u(_C_,sh), scvf.global_grad(sh));
-
-	//	compute the lin defect at this ip
-		MathMatrix<dim,dim> linDefect;
-
-	//	part coming from -\nabla u * \vec{n}
-		for(size_t k=0; k < (size_t)dim; ++k)
-			for(size_t j = 0; j < (size_t)dim; ++j)
-				linDefect(j,k) = (scvf.normal())[j] * grad_u[k];
-
-	//	add contribution from convection shapes
-		if(convShape.non_zero_deriv_diffusion())
-			for(size_t sh = 0; sh < scvf.num_sh(); ++sh)
-				MatAdd(linDefect, convShape.D_diffusion(ip, sh), u(_C_, sh));
-
-	//	add contributions
-		vvvLinDef[ip][_C_][scvf.from()] -= linDefect;
-		vvvLinDef[ip][_C_][scvf.to()  ] += linDefect;
-	}
-}
 
 template<typename TDomain>
 template <typename TElem>
@@ -498,15 +338,6 @@ lin_def_source(const LocalVector& u,
 
 
 ////////////////////////////////////////////////////////////////////////////////
-//	upwind
-////////////////////////////////////////////////////////////////////////////////
-
-template<typename TDomain>
-void ConservationLawFV<TDomain>::
-set_upwind(SmartPtr<IConvectionShapes<dim> > shapes) {m_spConvShape = shapes;}
-
-
-////////////////////////////////////////////////////////////////////////////////
 //	Value and Gradient
 ////////////////////////////////////////////////////////////////////////////////
 template<typename TDomain>
@@ -525,8 +356,6 @@ ex_value(number vValue[],
 {
 	typedef FV1Geometry<TElem, dim> TFVGeom;
 	
-	TElem * pElem = static_cast<TElem*> (elem);
-	
 // 	update the FV geometry for this element
 	TFVGeom& geo = GeomProvider<TFVGeom>::get ();
 
@@ -535,7 +364,6 @@ ex_value(number vValue[],
 
 //	number of shape functions
 	static const size_t numSH =	ref_elem_type::numCorners;
-
 
 //	SCVF ip
 	if(vLocIP == geo.scvf_local_ips())
@@ -653,7 +481,6 @@ ex_grad(MathVector<dim> vValue[],
 //	number of shape functions
 	static const size_t numSH =	ref_elem_type::numCorners;
 
-
 //	SCVF ip
 	if(vLocIP == geo.scvf_local_ips())
 	{
@@ -760,7 +587,6 @@ void ConservationLawFV<TDomain>::register_loc_discr_func ()
 	this->set_add_jac_M_elem_fct(id, &T::template ass_JM_elem<TElem>);
 	this->set_add_rhs_elem_fct	(id, &T::template ass_rhs_elem<TElem>);
 	
-	m_imDiffusion.set_fct(id, this, &T::template lin_def_diffusion<TElem>);
 	m_imFlux.set_fct(id, this, &T::template lin_def_flux<TElem>);
 	m_imSource.	  set_fct(id, this, &T::template lin_def_source<TElem>);
 	
@@ -781,8 +607,7 @@ ConservationLawFV
 )
 :	IElemDisc<TDomain> (functions, subsets),
 	m_exValue(new DataExport<number, dim>(functions)),
-	m_exGrad(new DataExport<MathVector<dim>, dim>(functions)),
-	m_spConvShape(new ConvectionShapesNoUpwind<dim>)
+	m_exGrad(new DataExport<MathVector<dim>, dim>(functions))
 {
 //	check number of functions
 	if (this->num_fct () != 1)
@@ -799,7 +624,6 @@ ConservationLawFV
 	register_all_loc_discr_funcs ();
 }
 
-	
 ////////////////////////////////////////////////////////////////////////////////
 //	user data
 ////////////////////////////////////////////////////////////////////////////////
@@ -818,44 +642,11 @@ void ConservationLawFV<TDomain>::
 init_imports()
 {
 	//	register imports
-		this->register_import(m_imDiffusion);
-		this->register_import(m_imFlux);
-		this->register_import(m_imSource);
+	this->register_import(m_imFlux);
+	this->register_import(m_imSource);
 	
-		m_imSource.set_rhs_part();
+	m_imSource.set_rhs_part();
 }
-
-
-
-//////// Diffusion
-
-template<typename TDomain>
-void ConservationLawFV<TDomain>::
-set_diffusion(SmartPtr<CplUserData<MathMatrix<dim, dim>, dim> > user)
-{
-	m_imDiffusion.set_data(user);
-}
-
-template<typename TDomain>
-void ConservationLawFV<TDomain>::set_diffusion(number val)
-{
-	if(val == 0.0) set_diffusion(SmartPtr<CplUserData<MathMatrix<dim, dim>, dim> >());
-	else set_diffusion(make_sp(new ConstUserMatrix<dim>(val)));
-}
-
-#ifdef UG_FOR_LUA
-template<typename TDomain>
-void ConservationLawFV<TDomain>::set_diffusion(const char* fctName)
-{
-	set_diffusion(LuaUserDataFactory<MathMatrix<dim,dim>, dim>::create(fctName));
-}
-template<typename TDomain>
-void ConservationLawFV<TDomain>::set_diffusion(LuaFunctionHandle fct)
-{
-	set_diffusion(make_sp(new LuaUserData<MathMatrix<dim,dim>, dim>(fct)));
-}
-#endif	
-
 
 //////// Flux
 
@@ -867,13 +658,13 @@ set_flux(SmartPtr<CplUserData<MathVector<dim>, dim> > user)
 }
 
 template<typename TDomain>
-void ConservationLawFV<TDomain>::set_flux(const std::vector<number>& vVel)
+void ConservationLawFV<TDomain>::
+set_flux(const std::vector<number>& vVel)
 {
 	bool bZero = true;
 	for(size_t i = 0; i < vVel.size(); ++i){
 		if(vVel[i] != 0.0) bZero = false;
 	}
-
 	if(bZero) set_flux(SmartPtr<CplUserData<MathVector<dim>, dim> >());
 	else set_flux(SmartPtr<ConstUserVector<dim> >(new ConstUserVector<dim>(vVel)));
 }
@@ -885,6 +676,7 @@ set_flux(const char* fctName)
 {
 	set_flux(LuaUserDataFactory<MathVector<dim>,dim>::create(fctName));
 }
+
 template<typename TDomain>
 void ConservationLawFV<TDomain>::
 set_flux(LuaFunctionHandle fct)
@@ -927,11 +719,9 @@ set_source(LuaFunctionHandle fct)
 }
 #endif
 
-
 ////////////////////////////////////////////////////////////////////////////////
 //	explicit template instantiations
 ////////////////////////////////////////////////////////////////////////////////
-
 #ifdef UG_DIM_1
 template class ConservationLawFV<Domain1d>;
 #endif
@@ -941,7 +731,6 @@ template class ConservationLawFV<Domain2d>;
 #ifdef UG_DIM_3
 template class ConservationLawFV<Domain3d>;
 #endif
-	
 	
 } // end namespace Conservation_Law_FV
 } // end namespace ug
